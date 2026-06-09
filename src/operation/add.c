ca1a6d3e4315638d0a1d50aca45e56fcfc9f2f32
Florian HEYTE <florian.heyte@epitech.eu>
2026-06-09 16:14:48 +0200

    [FEAT] add fonction with utils for it [src/operation/add.c src/operation/utils.c]

---
+++ src/operation/add.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2026
+** Bistro-matic-Mario-Co
+** File description:
+** add.c
+*/
+
+#include <stdlib.h>
+
+int get_digit_val(char *nb, int len, int i, char *base)
+{
+    if (i < len)
+        return (get_index_in_base(nb[len - 1 - i], base));
+    return (0);
+}
+
+char *my_add(char *nb1, char *nb2, char *base)
+{
+    int len1 = my_strlen(nb1);
+    int len2 = my_strlen(nb2);
+    int b_len = my_strlen(base);
+    int carry = 0;
+    int i = 0;
+    int sum = 0;
+    char *result = malloc(sizeof(char) * (len1 + len2 + 2));
+
+    for (; i < len1 || i < len2 || carry; i++) {
+        sum = carry;
+        sum += get_digit_val(nb1, len1, i, base);
+        sum += get_digit_val(nb2, len2, i, base);
+        result[i] = base[sum % b_len];
+        carry = sum / b_len;
+    }
+    result[i] = '\0';
+    return (my_revstr(result));
+}
