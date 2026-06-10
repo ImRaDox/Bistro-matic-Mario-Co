ed96fb6358ced5b92e1618fea982094e1017247b
Esteban Perez <esteban.perez@epitech.eu>
2026-06-10 16:39:28 +0200

    [FEAT] parse_expr need the implementations of my_add and my_sub [src/parser/parse_expr.c]

---
+++ src/parser/parse_expr.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2026
+** Bistro-matic-Mario-Co
+** File description:
+** parse_expr
+*/
+
+#include "bistromatic.h"
+
+char *parse_expr(char **str, char const *base, char const *ops)
+{
+    char *a = parse_term(str, base, ops);
+    char op;
+    char *b;
+
+    while (**str == ops[OP_PLUS_IDX] || **str == ops[OP_SUB_IDX]) {
+        op = **str;
+        (*str)++;
+        b = parse_term(str, base, ops);
+        if (op == ops[OP_PLUS_IDX])
+        /*Addition*/
+            a = ;
+        else
+        /*Soustraction*/
+            a = ;
+    }
+    return a;
+}
